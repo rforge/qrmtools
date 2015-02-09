4ca903f8296ecb663936a95d2717b41b37e46cd8
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-09 05:02:08 +0000

    updated demo to new RA interface
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qrmtools@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/demo/worst_VaR.R
+++ pkg/demo/worst_VaR.R
@@ -179,9 +179,9 @@ for(i in seq_len(n.th)) {
                                    pF=pFs[[i]])
     ## Rearrangement Algorithm
     set.seed(271) # use the same sampling for each theta
-    RA.res <- RA(alpha, d=d, qF=qFs[[i]], eps=0.01, N=N) # with 1% relative error
-    res[i,"RA.low"] <- RA.res[1]
-    res[i,"RA.up"]  <- RA.res[2]
+    RA. <- RA(alpha, d=d, qF=qFs[[i]], eps=0.01, N=N) # with 1% relative error
+    res[i,"RA.low"] <- RA.$bounds[1]
+    res[i,"RA.up"]  <- RA.$bounds[2]
 }
 
 ## Plot
