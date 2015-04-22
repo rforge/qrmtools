ce041fb24fbc5067baba7de1efd77b60011a6ffb
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-04-22 20:48:45 +0000

    added Black Scholes formula and Greeks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qrmtools@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Black_Scholes.R
+++ pkg/R/Black_Scholes.R
@@ -1,4 +1,17 @@
-BlackScholes <- function(t, S, r, sigma, K, T, type=c("call", "put"))
+### Compute the Black--Scholes formula and the Greeks
+
+##' @title Compute the Black--Scholes formula
+##' @param t initial/current time (in years)
+##' @param S Stock price at time t
+##' @param r risk-free annual interest rate
+##' @param sigma annual volatility (standard deviation)
+##' @param K strike
+##' @param T maturity (in years)
+##' @param type character string indicating whether the price
+##'        of a call or put option is to be computed
+##' @return option price
+##' @author Marius Hofert
+Black_Scholes <- function(t, S, r, sigma, K, T, type=c("call", "put"))
 {
     d1 <- (log(S/K) + (r+sigma^2/2)*(T-t))/(sigma*sqrt(T-t))
     d2 <- d1-sigma*sqrt(T-t)
@@ -13,7 +26,16 @@ BlackScholes <- function(t, S, r, sigma, K, T, type=c("call", "put"))
            stop("Wrong type"))
 }
 
-BlackScholesGreeks <- function(t, S, r, sigma, K, T)
+##' @title Compute the Greeks
+##' @param t initial/current time (in years)
+##' @param S Stock price at time t
+##' @param r risk-free annual interest rate
+##' @param sigma annual volatility (standard deviation)
+##' @param K strike
+##' @param T maturity (in years)
+##' @return Greeks
+##' @author Marius Hofert
+Black_Scholes_Greeks <- function(t, S, r, sigma, K, T)
 {
     d1 <- (log(S/K) + (r+sigma^2/2)*(T-t))/(sigma*sqrt(T-t))
     d2 <- d1-sigma*sqrt(T-t)
@@ -22,7 +44,7 @@ BlackScholesGreeks <- function(t, S, r, sigma, K, T)
         theta = -(S*dnorm(d1)*sigma)/(2*sqrt(T-t)) - r*K*exp(-r*(T-t))*pnorm(d2),
         rho = K*(T-t)*pnorm(d2)*exp(-r*(T-t)),
         vega = S*dnorm(d1)*sqrt(T-t),
-        ## Second-order
+        ## Second-order (only the most important ones)
         gamma = dnorm(d1)/(S*sigma*sqrt(T-t)),
         vanna = -dnorm(d1)*d2/sigma,
         vomma = S*dnorm(d1)*sqrt(T-t)*d1*d2/sigma)
