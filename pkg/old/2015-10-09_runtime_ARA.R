e804bac3f4fc0282bd37a150e2d02b3aa097d077
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-10-10 04:56:59 +0000

    old run time measurement file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qrmtools@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/old/2015-10-09_runtime_ARA.R
@@ -0,0 +1,26 @@
+## svn co svn+ssh://khornik@r-forge.r-project.org/svnroot/qrmtools
+require(qrmtools)
+
+alpha <- 0.99
+d <- 128
+N <- 2^14 # = 16384
+qF <- function(p, th=2) qPar(p, theta=th)
+method <- "worst"
+maxiter <- Inf
+
+qF <- rep(list(qF), d)
+p <- if(method=="worst") alpha + (1-alpha)*(N-1):0/N else alpha*(N-1):0/N
+X <- sapply(qF, function(qF) qF(p))
+
+## rearrange()
+Rprof(profiling <- tempfile())
+res <- rearrange(X)
+Rprof(NULL)
+summaryRprof(profiling)
+
+## ARA()
+Rprof(profiling <- tempfile())
+res <- ARA(alpha, qF=qF)
+Rprof(NULL)
+summaryRprof(profiling)
+
