2474cb74f794fa45582511ff3dd40592cceab7d2
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-11-10 01:23:44 +0000

    last commit before release
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qrmtools@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/old/2015-10-04_interface_C.R
+++
@@ -1,6 +0,0 @@
-For former implementation in C
-if(is.null(tol)) tol <- -1 # for C code
-if(is.infinite(maxiter)) maxiter <- -1 # for C code
-rearrange <- NULL # to avoid "RA_aux: no visible binding for global variable 'rearrange_'"
-.Call("rearrange_", X, method, tol.type, maxiter, tol)
-
