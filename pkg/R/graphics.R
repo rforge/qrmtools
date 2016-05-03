127e880b612917db3c0301a968e56baca6fcc969
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-03 00:37:24 +0000

    fixing colorization in plot_matrix()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qrmtools@124 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/graphics.R
+++ pkg/R/graphics.R
@@ -37,6 +37,7 @@ plot_NA <- function(x, col = c("black", "white"), xlab = "Time", ylab = "Compone
 ##' @param scales See levelplot(); if NULL, labels and ticks are omitted
 ##' @param at See levelplot()
 ##' @param colorkey See levelplot()
+##' @param col The default colors for the color key
 ##' @param col.regions See levelplot()
 ##' @param ... Additional arguments passed to levelplot()
 ##' @return The level plot
@@ -44,22 +45,36 @@ plot_NA <- function(x, col = c("black", "white"), xlab = "Time", ylab = "Compone
 ##' @note Another option would be:
 ##'       corrplot::corrplot(err, method="color", col=grey(seq(0.4, 1, length.out=200)),
 ##'                          tl.col="black", is.corr=FALSE)
-plot_matrix <- function(x, xlab="Column", ylab="Row",
-                        scales=list(alternating=c(1,1), tck=c(1,0), x=list(rot=90)),
-                        at=seq(min(x, na.rm=TRUE), max(x, na.rm=TRUE), length.out=200),
-                        colorkey=list(at=at),
-                        col.regions=colorRampPalette(c("royalblue3", "white", "maroon3"),
-                                                     space="Lab")(200),
-                        ...)
+plot_matrix <- function(x, xlab = "Column", ylab = "Row",
+                        scales = list(alternating = c(1,1), tck = c(1,0), x = list(rot = 90)),
+                        at = NULL, colorkey = NULL, col = c("royalblue3", "white", "maroon3"),
+                        col.regions = NULL, ...)
 {
     stopifnot(is.matrix(x), (nr <- nrow(x)) >= 1,
-              is.null(scales) || is.list(scales), is.numeric(at),
-              is.list(colorkey))
+              is.null(scales) || is.list(scales), is.numeric(at) || is.null(at),
+              is.list(colorkey) || is.null(colorkey), length(col) == 3)
     ran <- range(x, na.rm=TRUE)
+    if(all(ran >= 0)) {
+        if(is.null(at)) at <- seq(0, ran[2], length.out = 200)
+        if(is.null(col.regions))
+           col.regions <- colorRampPalette(c(col[2], col[3]), space = "Lab")(200)
+    } else if(all(ran <= 0)) {
+        if(is.null(at)) at <- seq(ran[1], 0, length.out = 200)
+        if(is.null(col.regions))
+           col.regions <- colorRampPalette(c(col[1], col[2]), space = "Lab")(200)
+    } else { # ran[1] < 0 && ran[2] > 0
+        ## Scale so that 0 gets the 'middle' color col[2]
+        frac1 <- -ran[1]/diff(ran)
+        frac2 <- ran[2]/diff(ran) # => frac1 + frac2 = 1 (and both in [0,1])
+        if(is.null(at)) at <- seq(ran[1], ran[2], length.out = 200)
+        if(is.null(col.regions))
+            col.regions <- c(colorRampPalette(c(col[1], col[2]), space = "Lab")(floor(200*frac1)),
+                             colorRampPalette(c(col[2], col[3]), space = "Lab")(ceiling(200*frac2)))
+    }
     if(min(x, na.rm=TRUE) < at[1] || max(x, na.rm=TRUE) > at[length(at)])
         stop("'x' values outside the range spanned by 'at'. Choose 'at' appropriately.")
     levelplot(t(x)[,nr:1], xlab=xlab, ylab=ylab,
               col.regions=col.regions,
               scales=if(is.null(scales)) list(alternating=c(0,0), tck=c(0,0)) else scales,
-              at=at, colorkey=colorkey, ...)
+              at=at, colorkey=if(is.null(colorkey)) list(at=at) else colorkey, ...)
 }
